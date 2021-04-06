73caee6ce127f2f5f810163a315b801bbcdb8b8b
Ryan Donnelly <ryanjdonnelly@gmail.com>
2021-04-05 22:36:02 -0400

    update home page

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,3 +1,32 @@
-import Demo from '@shared/components/Demo';
+import Head from 'next/head';
+import React from 'react';
+import styled from 'styled-components/native';
 
-export default Demo;
+const IndexPage = () => {
+  return (
+    <>
+      <Head>
+        <title>MC Builder</title>
+        <meta name="apple-itunes-app" content="app-id=1516561943" />
+      </Head>
+
+      <Container>
+        <img
+          src="/images/mc-icon-1024.png"
+          alt="MC Builder"
+          title="MC Builder"
+          width="200"
+        />
+      </Container>
+    </>
+  );
+};
+
+const Container = styled.View`
+  align-items: center;
+  flex: 1 1 auto;
+  justify-content: center;
+  width: 100%;
+`;
+
+export default IndexPage;
