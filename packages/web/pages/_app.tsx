2ccdf41119d4188c4eee5b5e4733e7d7592205db
Ryan Donnelly <ryanjdonnelly@gmail.com>
2021-04-06 00:29:51 -0400

    add favicon link to head

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -21,6 +21,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>MC Builder</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link
           rel="preload"
           href="/fonts/marvel-icons.ttf"
